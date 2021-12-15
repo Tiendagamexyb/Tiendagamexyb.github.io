17429a9baf4fa938d70e750dea82a4b3b2c25770
Tiendagamexyb <tiendagamxyb@gmail.com>
2021-12-15 15:49:04 -0600

    Final

---
+++ js/init.js
@@ -0,0 +1,18 @@
+/*  Inicializa Firebase con la
+ * configuración del proyecto.
+ * Revisa la configuración en tu
+ * servidor de Firebase y cópiala
+ * aquí sustituyendo los
+ * asteriscos. Los campos deben
+ * quedar igual que en tu
+ * servidor. */
+// @ts-ignore
+firebase.initializeApp({
+  apiKey: "AIzaSyDBNdx_vbkKN-TC084koDLvk2BeAZ6uBBk",
+  authDomain: "gamexybstore.firebaseapp.com",
+  projectId: "gamexybstore",
+  storageBucket: "gamexybstore.appspot.com",
+  messagingSenderId: "208230694826",
+  appId: "1:208230694826:web:335e72d61a20a3f4b5f5d3",
+  measurementId: "G-NNC4PEQP8J"
+});
